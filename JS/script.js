711c7c3420d04e3a2f3c2634eadca2f5b5709653
Amran-ahmed <amranahmed088@gmail.com>
2022-03-16 22:06:40 +0200

    added Jquery to portfolio images

--- JS/script.js
+++ JS/script.js
@@ -26,19 +26,61 @@ $(document).ready(function() {
     $("#product-text").click(function() {
         $("#product-text").hide("1000");
         $("#product").show();
- });
+   });
 });
- $(document).ready(function(){
+$(document).ready(function(){
     $("#firstimg").mouseover(function(){
-          $("#deli1").show();
-        })
-        $("#firstimg").mouseout(function(){
-          $("#deli1").hide();
-        });
+      $("#deli1").show();
+    })
+    $("#firstimg").mouseout(function(){
+      $("#deli1").hide();
+    });
     $("#secondimg").mouseover(function(){
         $("#deli2").show();
-    })
-    $("#secondimg").mouseout(function(){
-        $("#del2").hide();
+      })
+      $("#secondimg").mouseout(function(){
+        $("#deli2").hide();
+      });
+      $("#thirdimg").mouseover(function(){
+        $("#deli3").show();
+      })
+      $("#thirdimg").mouseout(function(){
+        $("#deli3").hide();
+      });
+      $("#fourthimg").mouseover(function(){
+        $("#deli4").show();
+      })
+      $("#fourthimg").mouseout(function(){
+        $("#deli4").hide();
+      });
+      $("#fifthimg").mouseover(function(){
+        $("#deli5").show();
+      })
+      $("#fifthimg").mouseout(function(){
+        $("#deli5").hide();
+      });
+      $("#sixthimg").mouseover(function(){
+        $("#deli6").show();
+      })
+      $("#sixthimg").mouseout(function(){
+        $("#deli6").hide();
+      });
+      $("#seventhimg").mouseover(function(){
+        $("#deli7").show();
+      })
+      $("#seventhimg").mouseout(function(){
+        $("#deli7").hide();
+      });
+      $("#eighthimg").mouseover(function(){
+        $("#deli8").show();
+      })
+      $("#eighthimg").mouseout(function(){
+        $("#deli8").hide();
+      });
+  });
+    $(document).ready(function () {
+        $(".button").click(function () {
+            alert("Thank you for Contacting us! We value you");
+        });
+        
     });
-});
